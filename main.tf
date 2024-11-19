a9273e634c60db20113d791e68585c3c72b70bdf
augieschwer <augie@schwer.us>
2024-11-19 13:17:59 -0800

    initial cleanup and make localstack friendly

--- main.tf
+++ main.tf
@@ -1,21 +1,5 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = "ami-785db401"
   instance_type = "t3.nano"
 
   tags = {
--- providers.tf
+++ providers.tf
@@ -2,10 +2,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
+  profile = "localstack"
+  endpoints {
+    sts            = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+  }
 }
