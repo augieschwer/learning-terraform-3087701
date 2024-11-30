4355d0ac2e4ab0f53350f759ef13ba1b6237315d
augieschwer <augie@schwer.us>
2024-11-30 15:42:09 -0800

    add alb even though localstack doesn't support it on non-pro images

--- main.tf
+++ main.tf
@@ -26,7 +26,43 @@ resource "aws_instance" "blog" {
   subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "blog"
+  }
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name = "blog-alb"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
+
+  target_groups = {
+    blog-instance = {
+      name_prefix = "blog"
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "instance"
+      target_id   = aws_instance.blog.id
+    }
+  }
+
+  listeners = {
+    blog-http = {
+      port     = 80
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "blog-instance"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "Blog"
   }
 }
 
--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.77"
+      source = "hashicorp/aws"
     }
   }
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
