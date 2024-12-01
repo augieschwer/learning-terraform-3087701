23e19baa5ff40df755e34d1f596e02cbfc365f12
augieschwer <augie@schwer.us>
2024-11-30 16:59:03 -0800

    add some variables

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ data "aws_vpc" "default" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -33,8 +33,8 @@ resource "aws_instance" "blog" {
 module "autoscaling" {
   source              = "terraform-aws-modules/autoscaling/aws"
   name                = "blog"
-  min_size            = 1
-  max_size            = 2
+  min_size            = var.asg_min_size
+  max_size            = var.asg_max_size
   vpc_zone_identifier = module.blog_vpc.public_subnets
   security_groups     = [module.blog_sg.security_group_id]
   image_id            = "ami-785db401"
